d2352ee5d679175f6e0c3ee7aae8e7017e0bb8b8
Andrew <8879789+manleyac@users.noreply.github.com>
2019-03-14 23:25:10 -0400

    ~C-language-basics~ First commit Adding Hello World and Variable examples

---
+++ variables/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   //bracket used after variable name for strings instead of a char
+   char name[] = "Andrew";
+   int age = 24;
+   printf("%s is %i\n", name, age);
+   return 0;
+}
