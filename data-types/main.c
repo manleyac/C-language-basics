dc4399db4589a8195b2db54af3b10e9a9c024e1b
Andrew <8879789+manleyac@users.noreply.github.com>
2019-03-15 22:49:08 -0400

    adding data type and printf examples

---
+++ data-types/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   int age = 24;
+   double gpa = 3.5;
+   char grade = 'A';
+   char phrase[] = "C programming";
+   return 0;
+}
---
+++ data-types/printf/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   const double NUM = 3.14;
+   printf("My favorite %s is %f\n ", "number", NUM);
+   return 0;
+};
